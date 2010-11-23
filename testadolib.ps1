fad246cb1832049e4bdc0465aeba4e93aed1408c
MikeShepard <MikeShepard@localhost>
2010-11-23 04:14:03 +0000

    Added copy-hashtable and used to exclude "AsResult" when splatting to new-sqlcommand.

--- testadolib.ps1
+++ testadolib.ps1
@@ -53,10 +53,22 @@ AssertEquals $rows.Count 7 "shared connection and SQL login"
 $rows=@(invoke-query -sql 'select * from Application where Application like @app' -conn $conn  -parameters @{app='CRM%'})
 AssertEquals $rows.Count 1 "parameterized query with shared connection and sql login" 
  
-#test stored procedure query with shared connection and sql login
+#test stored procedure query with shared connection and sql login and IN parameters
 $rows=@(invoke-storedprocedure  -storedProcName stp_TestInParam  -conn $conn  -parameters @{app='CRM%'})
 AssertEquals $rows.Count 1 "parameterized query (in) with shared connection and sql login" 
 
-#test stored procedure query with shared connection and sql login
+#test stored procedure query with shared connection and sql login and out parameters
 $outRows=@(invoke-storedprocedure  -storedProcName stp_TestOutParam  -conn $conn  -parameters @{app='CRM%'} -outparameters @{appID='uniqueidentifier'})
 AssertEquals ($outRows[0].appID -is [Guid]) $true "parameterized query (out) with shared connection and sql login" 
+
+#test NULL parameters
+$rows=invoke-query "select * from Application where @parm is NULL" -conn $conn -parameters @{parm=[System.DBNull]::Value}
+AssertEquals $rows.Count 7 "shared connection null parameters" 
+
+#test simple query using ad hoc connection and SQL Login with "-AsResult DataTable"
+$rows=invoke-query -sql $sql -server $server -database $db -user test_login -password 12345 -AsResult DataTable
+AssertEquals ($rows -is [Data.DataTable]) $true  "ad hoc connection with SQL Login as DataTable" 
+
+#test simple query using ad hoc connection and SQL Login with "-AsResult DataSet"
+$rows=invoke-query -sql $sql -server $server -database $db -user test_login -password 12345 -AsResult DataTable
+AssertEquals ($rows -is [Data.DataSet]) $true  "ad hoc connection with SQL Login as DataSet" 
